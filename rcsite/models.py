b961e8bdc1901729abeb6a6615f9960409daad30
Jasmine Mithani <jazzmithani@gmail.com>
2019-06-11 05:03:30 -0500

    adding movie model

--- rcsite/models.py
+++ rcsite/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Movie(model.Model):
+    title = models.CharField(max_length=256, null=True)
+    answer = models.BooleanField(null=True)
+    poster_link = models.CharField(max_length=1000, blank=True, null=True)
