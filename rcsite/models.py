5a5747d0ea8627e6bb79da9a6138133328cf4fc8
Jasmine Mithani <jazzmithani@gmail.com>
2019-06-11 05:14:22 -0500

    adding migration and admin

--- rcsite/models.py
+++ rcsite/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Movie(model.Model):
+class Movie(models.Model):
     title = models.CharField(max_length=256, null=True)
     answer = models.BooleanField(null=True)
     poster_link = models.CharField(max_length=1000, blank=True, null=True)
--- rcsite/views.py
+++ rcsite/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Movie
 
-# Create your views here.
+class IndexView(views.TemplateView):
+    template = 'index.html'
+
+    def get_context_data(self):
+        context['movies'] = Movie.objects.all()
+        return context
